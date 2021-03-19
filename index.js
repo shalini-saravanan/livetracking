f656ec0b49718dfa220a4490dc2734cf884c09d8
Shalini S <79892397+shalini-saravanan@users.noreply.github.com>
2021-03-20 00:25:50 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
+ if (user) {
     // User is signed in.
 
-    document.getElementById("user_div").style.display = "block";
-    document.getElementById("login_div").style.display = "none";
-
-    var user = firebase.auth().currentUser;
-
-    if(user != null){
-
-      var email_id = user.email;
-      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
-
-    }
-
+    window.location.href="product/shop-list.html";
+     
   } else {
     // No user is signed in.
 
@@ -21,7 +11,7 @@ firebase.auth().onAuthStateChanged(function(user) {
     document.getElementById("login_div").style.display = "block";
 
   }
-});
+}); 
 
 function login(){
 
@@ -32,14 +22,11 @@ function login(){
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
-
     window.alert("Error : " + errorMessage);
-
     // ...
   });
 
+
 }
 
-function logout(){
-  firebase.auth().signOut();
-}
+
